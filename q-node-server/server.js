7d2f9f0b8f4fd133d6c11f67655375914f28639b
Quinn Averill <qsaverill@wpi.edu>
2018-09-23 22:43:50 -0400

    Made a nice server logger

--- q-node-server/server.js
+++ q-node-server/server.js
@@ -4,20 +4,9 @@ const bodyParser = require('body-parser');
 require('dotenv').load();
 
 server.use(require('cors')());
+server.use(require('./logRequests'));
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
-
-server.use(function(req, res, next) {
-  const timestamp = new Date();
-  console.log('%s-%s: %s %s %s',
-    timestamp.toLocaleDateString(),
-    timestamp.toLocaleTimeString(),
-    req.method,
-    req.originalUrl,
-    req.method === "GET" ? JSON.stringify(req.query) : JSON.stringify(req.body)
-  );
-  next();
-});
 
 server.use('/aws', require('./aws'));
 server.use('/spotify', require('./spotify'));
