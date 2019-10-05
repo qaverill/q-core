73bd10f2e1455265e7a093e7b8dcd183eefe7436
Quinn Averill <qaverill15@gmail.com>
2019-10-05 12:28:16 -0400

    wip

--- q-backend/index.js
+++ q-backend/index.js
@@ -24,12 +24,7 @@ server.use((req, res, next) => {
   next();
 });
 
-server.use('/spotify', require('./spotify'));server.use((req, res, next) => {
-  // q_logger.info("req", req);
-  // q_logger.info("res", res);
-  // q_logger.info("next", next);
-  q_logger.debug("HEYYYY")
-});
+server.use('/spotify', require('./spotify'));
 server.use('/mongodb', require('./mongodb'));
 server.use('/lifx', require('./lifx'));
 
